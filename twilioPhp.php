0f5e56ec3affb5237a6fdba3ef94816769becc95
Momshad Alvee <momshadalvee@gmail.com>
2015-06-10 22:46:23 +0100

    added the first files

--- twilioPhp.php
+++ twilioPhp.php
@@ -5,17 +5,17 @@ require_once('twilio-php-master/Services/Twilio.php'); // Loads the library
 // Your Account Sid and Auth Token from twilio.com/user/account
 $sid = "ACf3bff83741dbbac71cc86f19e4c78884"; 
 $token = "ec08fa98cd88723f9798ec9d82df60e7"; 
-$client = new Services_Twilio($sid, $token);
-// $http = new Services_Twilio_TinyHttp('https://api.twilio.com',
-// 		array('curlopts' => array(CURLOPT_SSL_VERIFYPEER => true,
-//         CURLOPT_SSL_VERIFYHOST => 2) ));
+//$client = new Services_Twilio($sid, $token);
+ $http = new Services_Twilio_TinyHttp('https://api.twilio.com',
+ 		array('curlopts' => array(CURLOPT_SSL_VERIFYPEER => true,
+         CURLOPT_SSL_VERIFYHOST => 2) ));
 
 
 
 
-// $client = new Services_Twilio($sid, $token, "2010-04-01", $http);
+ $client = new Services_Twilio($sid, $token, "2010-04-01", $http);
  
-$call = $client->account->calls->create("+447949319540", "+441702806342", "http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient", array());
+$call = $client->account->calls->create( "+441702806342","+447949319540", "http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient", array());
 echo $call->sid;
 
 ?>
